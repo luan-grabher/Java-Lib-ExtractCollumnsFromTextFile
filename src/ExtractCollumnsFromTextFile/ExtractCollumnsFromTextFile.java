925d3205e516c277310f0c13c70a72a0b7a8ce84
luan-grabher <lujetix@gmail.com>
2020-04-28 16:08:45 -0300

    Merge origin/master
    Conflicts:
    	.gitignore

---
+++ src/ExtractCollumnsFromTextFile/ExtractCollumnsFromTextFile.java
@@ -0,0 +1,22 @@
+package ExtractCollumnsFromTextFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExtractCollumnsFromTextFile {
+    private String file;
+    
+    private List<List[]> mapCollumns;
+
+    public ExtractCollumnsFromTextFile(String file, List<List[]> mapCollumns) {
+        this.file = file;
+        this.mapCollumns = mapCollumns;
+    }
+    
+    public List<List[]> getCollumns(){
+        List<List[]> list = new ArrayList<>();
+        
+        
+        return list;
+    }
+}
